48be53e744ce533c4bfe5baf0afca71e89d8add7
SUR18 Codex <sur18-codex@users.noreply.github.com>
2026-09-10 15:09:16 -0400

    fix: corregir parámetros y compatibilidad MariaDB en Save

--- ignition_project/SUR18_Solar_Designer/ignition/named-query/Designs/Save/query.sql
+++ ignition_project/SUR18_Solar_Designer/ignition/named-query/Designs/Save/query.sql
@@ -1,2 +1,2 @@
 INSERT INTO designs(project_id,revision,name,input_json,result_json,notes,created_by)
-VALUES (:project_id,:revision,:name,CAST(:input_json AS JSON),CAST(:result_json AS JSON),:notes,:user);
+VALUES (:project_id,:revision,:name,:input_json,:result_json,:notes,:user);
